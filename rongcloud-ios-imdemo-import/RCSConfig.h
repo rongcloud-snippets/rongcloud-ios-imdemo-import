8921fa7702a9fe46bfd29d21e839aacbf2b63b3d
张一帆 <iosyifan@163.com>
2020-08-12 09:53:00 +0800

    问题:  [fix] 1. 修改AppKey token 原因及方案: Reviewer: 来源:

--- rongcloud-ios-imdemo-import/RCSConfig.h
+++ rongcloud-ios-imdemo-import/RCSConfig.h
@@ -11,10 +11,10 @@
 
 // AppKey 获取地址 ： https://developer.rongcloud.cn/app/appkey/0vMK99Huzz-qw40Ybv4NDA
 // 选择 AppKey 需注意选中项目及选中环境，不同环境的 AppKey 不相同。
-#define RONGCLOUD_APPKEY @"cpj2xarlctx9n"
+#define RONGCLOUD_APPKEY @"xxxx"
 
 // 连接融云服务，需要传入对应的 token。 测试情况 token 可直接在开发者后台获取。
 // 获取 token ：https://developer.rongcloud.cn/apitool/Mw8EsJmV43kZBugTMSAZXg
-#define RONGCLOUD_TOKEN  @"gVS98KCTmKSYQQAnnQSgJ8Yyr4H2YxAT@b0fu.cn.rongnav.com;b0fu.cn.rongcfg.com"
+#define RONGCLOUD_TOKEN  @"xxxx"
 
 #endif /* RCSConfig_h */
