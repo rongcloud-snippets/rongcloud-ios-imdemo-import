7639d8b5b37b23c61c6667182ccd94007bd1ae49
wangjue <wangjue@rongcloud.cn>
2021-04-02 11:39:40 +0800

    修改脚本

--- before_build.sh
+++ before_build.sh
@@ -9,20 +9,20 @@ for str in `cat $1 | sed 's/ //g'`
 do
 	if [[ $str == *"IMKit','~>4"* || $str == *"IMKit','4"* ]] 
 	then
-		sed -i '' '/SDK_VERSION 2/d' $2
-		sed -i '' '/SDK_VERSION 5/d' $2
+		sed -i '' '/SDK_VERSION/d' $2
+		echo "#define SDK_VERSION 4" >> $2
 		exit
 	fi
 	if [[ $str == *"IMKit','~>2"* || $str == *"IMKit','2"* ]] 
 	then
-		sed -i '' '/SDK_VERSION 4/d' $2
-		sed -i '' '/SDK_VERSION 5/d' $2
+		sed -i '' '/SDK_VERSION/d' $2
+		echo "#define SDK_VERSION 2" >> $2
 		exit
 	fi
 	if [[ $str == *"IMKit','~>5"* || $str == *"IMKit','5"* ]] 
 	then
-		sed -i '' '/SDK_VERSION 2/d' $2
-		sed -i '' '/SDK_VERSION 4/d' $2
+		sed -i '' '/SDK_VERSION/d' $2
+		echo "#define SDK_VERSION 5" >> $2
 		exit
 	fi
 done
